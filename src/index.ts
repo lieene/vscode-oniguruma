647e18038d5d36cabad65437cdaba7bd18299bea
Lieene Guo <lieene@shaderealm.com>
2019-11-18 23:23:24 +0800

    1.1.0

--- src/index.ts
+++ src/index.ts
@@ -12,23 +12,18 @@ import { Tree, Name } from 'poly-tree';
 import * as L from '@lieene/ts-utility';
 import { promisify } from 'util';
 
-
-export interface VscodeLike
-{
+export interface VscodeLike {
   env: { appRoot: string };
 }
 
-namespace Internal
-{
-  export interface OniBin
-  {
+namespace Internal {
+  export interface OniBin {
     _findNextMatchSync(string: string, startPosition: number): Match | null;
     _findNextMatch(string: string, startPosition: number, callBack: (error: any, match: Match | null) => void): void;
   }
 
-  interface OniBinCtor
-  {
-    new(patterns: string[]): OniBin;
+  interface OniBinCtor {
+    new (patterns: string[]): OniBin;
   }
 
   var binCtor: OniBinCtor = L.Uny;
@@ -38,64 +33,46 @@ namespace Internal
 
   export function InitFrom(vscode: VscodeLike): void;
   export function InitFrom(fullpath: string): void;
-  export function InitFrom(from: VscodeLike | string): void
-  {
-    try
-    {
-      if (L.IsString(from))
-      {
+  export function InitFrom(from: VscodeLike | string): void {
+    try {
+      if (L.IsString(from)) {
         binCtor = require(from).OnigScanner;
-      } else
-      {
+      } else {
         binCtor = require(from.env.appRoot + pathInVsRoot).OnigScanner;
       }
-    } catch (e)
-    {
+    } catch (e) {
       GetOni();
     }
   }
 
-  export function GetOni(): OniBinCtor
-  {
-    if (binCtor === undefined)
-    {
-      try
-      {
+  export function GetOni(): OniBinCtor {
+    if (binCtor === undefined) {
+      try {
         binCtor = require(PathInPackage).OnigScanner;
-      } catch (e)
-      {
-        try
-        {
+      } catch (e) {
+        try {
           binCtor = require(localPath).OnigScanner;
-        } catch (e)
-        {
+        } catch (e) {
           throw new Error('Oniguruma binary node file not found. try use OnigScanner.InitFrom ahead.');
         }
       }
     }
     return binCtor;
   }
-  export function IsGroupFound(g0: GroupCapture, gn: GroupCapture): boolean
-  {
-    if (gn === undefined)
-    {
+  export function IsGroupFound(g0: GroupCapture, gn: GroupCapture): boolean {
+    if (gn === undefined) {
       return false;
     }
-    if (gn.index === 0)
-    {
+    if (gn.index === 0) {
       return true;
     }
-    if (gn.start === 0)
-    {
-      if (g0.start !== 0)
-      {
+    if (gn.start === 0) {
+      if (g0.start !== 0) {
         return false;
-      } else
-      {
+      } else {
         return gn.length > 0 || (g0.length === 0 && gn.length === 0);
       } //do not allow leading anchor
-    } else
-    {
+    } else {
       return true;
     }
   }
@@ -113,21 +90,18 @@ namespace Internal
   const patternsExt = [...patterns, '(?<!\\\\)#.*']; //8 lineEndComment
 
   var ops: Internal.OniBin | undefined = undefined;
-  function oniPatterScanner(): Internal.OniBin
-  {
+  function oniPatterScanner(): Internal.OniBin {
     return ops === undefined ? (ops = new (Internal.GetOni())(patterns)) : ops;
   }
 
   let opsx: Internal.OniBin | undefined = undefined;
-  function oniPatterScannerExt(): Internal.OniBin
-  {
+  function oniPatterScannerExt(): Internal.OniBin {
     return opsx === undefined ? (opsx = new (Internal.GetOni())(patternsExt)) : opsx;
   }
 
   let oop: Internal.OniBin | undefined = undefined;
   //https://regex101.com/r/sASjOR/1
-  function oniOption(): Internal.OniBin
-  {
+  function oniOption(): Internal.OniBin {
     return oop === undefined ? (oop = new (Internal.GetOni())(['\\?(?:[imWDSPy]|(x))*-?(?:[imWDSPy]|(x))*'])) : oop;
   }
 
@@ -142,8 +116,7 @@ namespace Internal
   let ConditionalGroup = 7;
   let lineEndComment = 8;
 
-  export function parseSource(source: string): Pattern
-  {
+  export function parseSource(source: string): Pattern {
     let groupTree: Tree.MorphTreeX<Name, { source: string }> = Tree<Name, { source: string }>({ source: source });
     let groupNode = groupTree.root;
     let cap: boolean = true;
@@ -152,15 +125,13 @@ namespace Internal
     let extStack: Array<boolean> = [];
     let position = 0;
     let match: Match | null = oniPatterScanner()._findNextMatchSync(source, position);
-    while (match !== null)
-    {
+    while (match !== null) {
       let captures = match.captureIndices;
       let fullMatch = captures[0];
       let g1 = captures[1];
       let index = match.index;
       position = fullMatch.end;
-      switch (index)
-      {
+      switch (index) {
         case indexCapGroup:
           cap = true;
           capStack.push(cap);
@@ -173,33 +144,27 @@ namespace Internal
           break;
         case groupEnd:
           cap = capStack.pop()!;
-          if (cap)
-          {
+          if (cap) {
             groupNode = groupNode.parent!;
           }
           break;
         case optionSwitch:
         case optionGroup:
           let matchOp = oniOption()._findNextMatchSync(source.slice(fullMatch.start, fullMatch.end), 0);
-          if (matchOp !== null)
-          {
+          if (matchOp !== null) {
             let newExt: boolean = ext;
-            if (matchOp.captureIndices[1].length === 1)
-            {
+            if (matchOp.captureIndices[1].length === 1) {
               newExt = true;
             }
-            if (matchOp.captureIndices[2].length === 1)
-            {
+            if (matchOp.captureIndices[2].length === 1) {
               newExt = false;
             }
-            if (index === optionGroup)
-            {
+            if (index === optionGroup) {
               extStack.push(ext);
               ext = newExt;
               cap = false;
               capStack.push(cap);
-            } else if (index === optionSwitch)
-            {
+            } else if (index === optionSwitch) {
               ext = newExt;
             }
           }
@@ -218,11 +183,9 @@ namespace Internal
         default:
           break;
       }
-      if (ext)
-      {
+      if (ext) {
         match = oniPatterScannerExt()._findNextMatchSync(source, position);
-      } else
-      {
+      } else {
         match = oniPatterScanner()._findNextMatchSync(source, position);
       }
     }
@@ -236,8 +199,7 @@ export type Callback<T> = (error: Error, match: T) => void;
  * An object representing a range within a search string, corresponding to
  * either a full-string match, or a capturing group within a match.
  */
-export interface GroupCapture
-{
+export interface GroupCapture {
   /** The index of the capturing group, or 0 for a full-string match */
   index: number;
   /** The position in the search string where the capture begins */
@@ -259,8 +221,7 @@ export interface GroupCapture
  * An object representing one successful regex match between a pattern and a
  * search string.
  */
-export interface Match
-{
+export interface Match {
   /** The index of the best pattern match */
   index: number;
 
@@ -279,37 +240,32 @@ export type MatchTree = Tree.MorphTreeX<MatchNodeExt, { source: string }>;
 export type MatchNode = Tree.MorphNodeX<MatchNodeExt, { source: string }>;
 
 type MatchNodeExt = { range: L.Range } & Name;
-function MatchNodeExt(range: L.Range, name?: string): MatchNodeExt
-{
+function MatchNodeExt(range: L.Range, name?: string): MatchNodeExt {
   let x = Object.assign(Name(name), { range: range });
   x.toString = LogMatchString;
   return x;
 }
 
-function LogMatchString(this: MatchNode): string
-{
+function LogMatchString(this: MatchNode): string {
   let slice = this.tree.source.slice(this.range.start, this.range.end);
   let rgs = this.range.toString();
-  return `${this.name === undefined ? '' : this.name + "|"}${rgs}:${slice}`;
+  return `${this.name === undefined ? '' : this.name + '|'}${rgs}:${slice}`;
 }
 
 /**
  * An object representing one OR MORE regex patterns, which can be used to
  * interrogate strings for matches against any of the supplied patterns.
  */
-export class OnigScanner
-{
+export class OnigScanner {
   /**
    * Create a new scanner with the given patterns.
    * @param patterns An array of string patterns.
    */
-  constructor(...patterns: string[])
-  {
+  constructor(...patterns: string[]) {
     this.internalOni = new (Internal.GetOni())(patterns);
     this.patterns = [];
     let pushable = this.patterns as Array<Pattern>;
-    for (let i = 0, len = patterns.length; i < len; i++)
-    {
+    for (let i = 0, len = patterns.length; i < len; i++) {
       pushable.push(Internal.parseSource(patterns[i]));
     }
   }
@@ -324,8 +280,7 @@ export class OnigScanner
    * @param startPosition The optional position to start at, defaults to 0
    * @return Promise a match or null if not found
    */
-  async findNextMatch(string: string, startPosition: number = 0): Promise<Match | null>
-  {
+  async findNextMatch(string: string, startPosition: number = 0): Promise<Match | null> {
     let m = await promisify<string, number, Match | null>(this.internalOni._findNextMatch)(string, startPosition);
     return this.filterMatch(string, m);
   }
@@ -336,53 +291,47 @@ export class OnigScanner
    * @param startPosition The optional position to start at, defaults to 0
    * @return An object containing details about the match, or null if no match
    */
-  findNextMatchSync(string: string, startPosition?: number): Match | null
-  {
-    if (startPosition === undefined)
-    {
+  findNextMatchSync(string: string, startPosition?: number): Match | null {
+    if (startPosition === undefined) {
       startPosition = 0;
     }
     return this.filterMatch(string, this.internalOni._findNextMatchSync(string, startPosition));
   }
 
-  async forAllMatch(string: string, onMatch: (m: Match) => void): Promise<void>
-  {
-    let m, pos = 0;
-    while (m = await this.findNextMatch(string, pos))
-    {
+  async forAllMatch(string: string, onMatch: (m: Match) => void): Promise<void> {
+    let m,
+      pos = 0;
+    while ((m = await this.findNextMatch(string, pos))) {
       onMatch(m);
       pos = m.captureIndices[0].end;
     }
   }
 
-  forAllMatchSync(string: string, onMatch: (m: Match) => void): void
-  {
-    let m, pos = 0;
-    while (m = this.findNextMatchSync(string, pos))
-    {
+  forAllMatchSync(string: string, onMatch: (m: Match) => void): void {
+    let m,
+      pos = 0;
+    while ((m = this.findNextMatchSync(string, pos))) {
       onMatch(m);
       pos = m.captureIndices[0].end;
     }
   }
 
-  async findAllMatch(string: string): Promise<Match[]>
-  {
-    let m, pos = 0;
+  async findAllMatch(string: string): Promise<Match[]> {
+    let m,
+      pos = 0;
     let am: Match[] = [];
-    while (m = await this.findNextMatch(string, pos))
-    {
+    while ((m = await this.findNextMatch(string, pos))) {
       am.push(this.filterMatch(string, m)!);
       pos = m.captureIndices[0].end;
     }
     return am;
   }
 
-  findAllMatchSync(string: string): Match[]
-  {
-    let m, pos = 0;
+  findAllMatchSync(string: string): Match[] {
+    let m,
+      pos = 0;
     let am: Match[] = [];
-    while (m = this.findNextMatchSync(string, pos))
-    {
+    while ((m = this.findNextMatchSync(string, pos))) {
       am.push(m);
       pos = m.captureIndices[0].end;
     }
@@ -390,36 +339,41 @@ export class OnigScanner
   }
 
   //BuildMatchTree<T extends object>(builder: (g: Tree.MorphNodeN<Named>, n: Tree.MorphNodeN<T>) => T, ...matches: Match[]): Tree.MorphTreeN<T>;
-  buildMatchTree(string: string): MatchTree | null
-  {
-    let info = { source: string, toString: () => { return string; } };
+  buildMatchTree(string: string): MatchTree | null {
+    let info = {
+      source: string,
+      toString: () => {
+        return string;
+      },
+    };
     let mt: MatchTree = Tree<MatchNodeExt, { source: string }>({ source: string });
     mt.root.poly(info);
     mt.source = string;
-    let m, pos = 0;
-    while (m = this.findNextMatchSync(string, pos))
-    {
+    let m,
+      pos = 0;
+    while ((m = this.findNextMatchSync(string, pos))) {
       let gpt: Tree.MorphTreeNX<Name> = this.patterns[m.index] as any;
       let caps = m.captureIndices;
-      let sub = gpt.clone(true, n => caps[n.index].isMatched, (n, o) =>
-      {
-        let c = caps[o.index];
-        n.poly(MatchNodeExt(L.StartLen(c.start, c.length), o.name));
-      });
+      let sub = gpt.clone(
+        true,
+        n => caps[n.index].isMatched,
+        (n, o) => {
+          let c = caps[o.index];
+          n.poly(MatchNodeExt(L.StartLen(c.start, c.length), o.name));
+        },
+      );
       mt.merg(sub, false);
       pos = caps[0].end;
     }
     return mt;
   }
 
-
   /**
    * Test if this regular expression matches the given string.
    * @param string The string to test against.
    * @return boolean Promise will be true if at least one match was found, or false otherwise.
    */
-  async test(string: string): Promise<boolean>
-  {
+  async test(string: string): Promise<boolean> {
     return (await promisify<string, number, Match | null>(this.internalOni._findNextMatch)(string, 0)) !== null;
   }
 
@@ -428,21 +382,17 @@ export class OnigScanner
    * @param string The string to test against.
    * @return True if there is at least one match, or false otherwise.
    */
-  testSync(string: string): boolean
-  {
+  testSync(string: string): boolean {
     return this.internalOni._findNextMatchSync(string, 0) === null;
   }
 
   private internalOni: Internal.OniBin;
-  private filterMatch(string: string, m: Match | null): Match | null
-  {
-    if (m !== null)
-    {
+  private filterMatch(string: string, m: Match | null): Match | null {
+    if (m !== null) {
       m.scanner = this;
       m.groupInfo = this.patterns[m.index];
       let g0 = m.captureIndices[0];
-      m.captureIndices.forEach((gn, i) =>
-      {
+      m.captureIndices.forEach((gn, i) => {
         gn.groudName = m.groupInfo.nodes[i].name;
         gn.isMatched = Internal.IsGroupFound(g0, gn);
         gn.match = string.slice(gn.start, gn.end);
